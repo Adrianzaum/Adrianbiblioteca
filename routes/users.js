4f39422f701fe0bb296ec22e455cc3a2a1068bb8
Adrianzaum <adriandalpont@gmail.com>
2023-07-27 12:12:48 -0300

    first commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/cool", function (req, res, next) {
+  res.send("You are so cool");
+});
+
+router.get("/cool/beach", function (req, res, next) {
+  res.send("I love beach");
+});
+
+router.get("/ifc", function (req, res, next) {
+  res.send("I love ifc");
 });
 
 module.exports = router;
